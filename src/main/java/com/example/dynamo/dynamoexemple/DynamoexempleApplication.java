f897403605c5daf4444e8d9e10ce99a60b374281
Bruno Eduardo <beduardo260@gmail.com>
2022-05-17 10:28:45 -0300

    first commit

---
+++ src/main/java/com/example/dynamo/dynamoexemple/DynamoexempleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamo.dynamoexemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoexempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoexempleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dynamo/dynamoexemple/DynamoexempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamo.dynamoexemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoexempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
